f850e49a15680106f29dc2e8e8d3af620d438173
Daniel P. Shannon <daniel.shannon@voxmedia.com>
2021-06-03 13:25:30 -0400

    Start feature lede.

--- components/TheCutFeatureLede.js
+++ components/TheCutFeatureLede.js
@@ -1,6 +1,10 @@
-export default function TheCutFeatureLede() {
+export default function TheCutFeatureLede({ heroCaption, heroImage, children, date, rubric }) {
     return (
-        <div className="max-w-6xl m-auto my-24 pt-4 border-t border-black">
+        <div className="max-w-4xl m-auto my-24 pt-4 border border-l-8 border-black">
+            <div className="uppercase text-xs mb-5"><span className="font-chap-bold">{ rubric }</span> <span className="text-red-500 px-2">|</span>  <span className="font-chap">{ date }</span></div>
+            <div>
+
+            </div>
         </div>
     )
 }
--- pages/thecut/feature.js
+++ pages/thecut/feature.js
@@ -1,7 +1,20 @@
+import Byline from '../../components/Byline'
+import Hed from '../../components/Hed'
 import TheCutFeatureLede from '../../components/TheCutFeatureLede'
 
 export default function TheCutFeature() {
-  return (
-    <TheCutFeatureLede />
-  )
+    return (
+        <TheCutFeatureLede
+            heroCaption="Photo: Bird"
+            heroImage="https://cdn.vox-cdn.com/thumbor/BwFVMS2O3XJc76I8CbarlKEjUQo=/0x0:7178x4788/1820x1213/filters:focal(3015x1820:4163x2968):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69350084/B3_Press.0.jpg"
+            date="May 27, 2021"
+            rubric="Rideables">
+            <Hed>
+                Bird announces its third-generation electric scooter with automatic emergency braking
+            </Hed>
+            <Byline>
+                By Andrew J. Hawkins
+            </Byline>
+        </TheCutFeatureLede>
+    )
 }
