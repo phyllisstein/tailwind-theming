19e33e13efbc9e68e87387ea0ffc4da31ab65876
ambikacastle <ambika.castle@voxmedia.com>
2021-06-07 16:32:44 -0700

    add more type scale values, tweak existing ones, apply them

--- components/TheCutLede.js
+++ components/TheCutLede.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function TheCutLede() {
     return (
         <div className="max-w-6xl m-auto my-24 pt-4 border-t border-black">
-            <div className="uppercase tracking-widest text-xs mb-5"><span className="font-chap-bold">Rideables</span> <span className="text-red-500 px-2">|</span>  <span className="font-chap">May 27, 2021</span></div>
-            <h1 className="font-canela text-4xl mb-2">
+            <div className="uppercase text-chap-xs mb-5"><span className="font-chap-bold">Rideables</span> <span className="text-red-500 px-2">|</span>  <span className="font-chap">May 27, 2021</span></div>
+            <h1 className="font-canela text-canela-l mb-2">
                 Bird announces its third-generation electric scooter with automatic emergency braking
             </h1>
-            <p className="font-chap text-sm border-b border-gray-300 pb-4 mb-8">By Andrew J. Hawkins</p>
+            <p className="font-chap text-chap-s border-b border-gray-300 pb-4 mb-8">By Andrew J. Hawkins</p>
             <figure>
                 <img className="w-2/3 mb-2" src="https://cdn.vox-cdn.com/thumbor/BwFVMS2O3XJc76I8CbarlKEjUQo=/0x0:7178x4788/1820x1213/filters:focal(3015x1820:4163x2968):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69350084/B3_Press.0.jpg"></img>
-                <figcaption className="font-chap text-gray-500 text-xs">Photo: Bird</figcaption>
+                <figcaption className="font-chap text-gray-500 text-chap-xs tracking-normal">Photo: Bird</figcaption>
             </figure>
       </div>
     )
--- components/VultureFeatureLede.js
+++ components/VultureFeatureLede.js
@@ -7,14 +7,14 @@ export default function VultureFeatureLede() {
             <div className="w-full border-l-8 pl-20 border-black">
                 <div className="flex flex-row">
                     <div className="w-1/2 flex flex-col justify-center pr-20">
-                    <div className="uppercase mb-5 tracking-widest"><span className="font-elderkin text-elderkin-xs ">In Conversation</span> <span className="text-blue-500 px-2">|</span>  <span className="font-miller-text text-miller-text-xs">May 27, 2021</span></div>
+                    <div className="uppercase mb-5"><span className="font-elderkin text-elderkin-xs ">In Conversation</span> <span className="text-blue-500 px-2">|</span>  <span className="font-miller-text text-miller-text-2xs">May 27, 2021</span></div>
                     <div className="block mb-4">
                     <h1 className="inline font-elderkin text-elderkin-4xl mr-2">
                         In Conversation: Francis Ford Coppola
                     </h1>
-                    <h2 className="inline font-miller-display text-5xl">His recut of The Godfather: Part III is one of his most personal projects yet, but it’s hardly his last.</h2>
+                    <h2 className="inline font-miller-display text-miller-display-xl">His recut of The Godfather: Part III is one of his most personal projects yet, but it’s hardly his last.</h2>
                     </div>
-                    <p className="font-miller-italic tracking-widest text-miller-text-xs pb-4 mb-8">By Bilge Ebiri <span className="text-blue-500 px-2">|</span> Photograph by Katy Grannan</p>
+                    <p className="font-miller-italic text-miller-text-xs pb-4 mb-8">By Bilge Ebiri <span className="text-blue-500 px-2">|</span> Photograph by Katy Grannan</p>
                     </div>
                     <figure className="w-1/2">
                         <img className="w-full h-full mb-2" src="https://pyxis.nymag.com/v1/imgs/74f/d38/457f4cfaffd76cd4b149b85c85b0592fc5-10-francis-ford-coppola-opener.2x.rvertical.w570.jpg"></img>
@@ -22,7 +22,7 @@ export default function VultureFeatureLede() {
                 </div>
                 </div>
         </div>
-        <figcaption className="font-miller-text text-miller-text-xs w-1/2 ml-auto pl-20">“I’ve always been doing weird, stupid things and following my heart. It made no sense to handle my career the way I did, but I wouldn’t do it another way.” <span class="text-gray-500">Photo: Katy Grannan for New York Magazine</span>
+        <figcaption className="font-miller-text text-miller-text-2xs w-1/2 ml-auto pl-20 tracking-wider">“I’ve always been doing weird, stupid things and following my heart. It made no sense to handle my career the way I did, but I wouldn’t do it another way.” <span class="text-gray-500">Photo: Katy Grannan for New York Magazine</span>
         </figcaption>
     </div>
     )
