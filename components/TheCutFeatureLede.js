bf8e3affee2c1369edb8a49e5e8b594a603d988a
ambikacastle <ambika.castle@voxmedia.com>
2021-06-09 13:00:16 -0700

    small tweak

--- components/TheCutFeatureLede.js
+++ components/TheCutFeatureLede.js
@@ -6,7 +6,7 @@ export default function TheCutFeatureLede() {
             <div className="border-b border-t border-r border-gray-400 mb-2">
             <div className="w-full border-l-12 pl-20 border-black">
                 <div className="flex flex-row">
-                    <div className="w-1/2 flex flex-col justify-center pr-20 max-w-lg">
+                    <div className="w-1/2 flex flex-col justify-center pr-20">
                     <div className="uppercase text-chap-3xs mb-5"><span className="font-chap-bold border-r border-red-500 pr-2">Rideables</span><span className="font-chap pl-3">May 27, 2021</span></div>
                     <div className="block mb-4">
                     <h1 className="font-canela-bold text-canela-3xl mb-1 inline mr-2">
