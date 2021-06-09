3b8183b6b7db650d7562bf01553480c6a8094a0f
ambikacastle <ambika.castle@voxmedia.com>
2021-06-09 16:07:55 -0700

    a few more styles

--- components/VoxFeatureLede.js
+++ components/VoxFeatureLede.js
@@ -7,9 +7,9 @@ export default function VoxFeatureLede() {
             <div className="w-full pl-20">
                 <div className="flex flex-row">
                     <div className="max-w-lg flex flex-col justify-center absolute top-1/4 left-1/4">
-                    <div className="uppercase text-xs mb-5"><span className="font-balto-bold pr-2 relative z-0 vox-highlight">Rideables</span> / <span className="font-balto pl-2 text-gray-500">May 27, 2021</span></div>
+                    <div className="text-xs mb-5"><span className="font-balto-bold pr-2 relative z-0 vox-highlight">Rideables</span> / <span className="font-balto pl-2 text-gray-500">May 27, 2021</span></div>
                     <h1 className="font-balto-bold text-balto-3xl mb-8"><span className="border-b-6 border-black pb-2">Bird announces its third-generation electric scooter with automatic emergency braking</span></h1>
-                    <p className="font-balto text-chap-2xs pb-4 mb-8">By <span className="relative z-0 vox-highlight">Andrew J. Hawkins</span></p>
+                    <p className="font-balto text-chap-2xs pb-4 mb-8">By <span className="relative z-0 vox-highlight font-balto-bold">Andrew J. Hawkins</span> @andyjayhawk</p>
                     </div>
                     <figure className="w-1/2 ml-auto">
                         <img className="w-full h-full mb-2" src="https://assets.codepen.io/495601/bird-vertical.png"></img>
